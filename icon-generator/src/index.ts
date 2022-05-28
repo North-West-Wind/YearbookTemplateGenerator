e729deac97e921776032b717b9f81f8880bbfa2f
NorthWestWind <wsyn148@gmail.com>
2022-05-28 22:49:59 +0800

    Changed some sequence

--- icon-generator/src/index.ts
+++ icon-generator/src/index.ts
@@ -6,19 +6,20 @@ if (!fs.existsSync("out") || !fs.statSync("out").isDirectory()) fs.mkdirSync("ou
 
 for (let ii = 1; ii <= 29; ii++) generator(ii);
 
-async function generator(index: number) {
-	const canvas = new Canvas(1080, 1080);
-	const ctx = canvas.getContext("2d");
-
-	ctx.save();
-	circle(ctx, 0, 0, 540, false);
-	ctx.clip();
+function generator(index: number) {
 	const icon = new Image();
 	icon.onload = () => {
 		const shorterSide = Math.min(icon.naturalWidth, icon.naturalHeight);
-		const width = icon.naturalWidth / shorterSide * 1080;
-		const height = icon.naturalHeight / shorterSide * 1080;
-		ctx.drawImage(icon, (1080 - width) / 2, (1080 - height) / 2, width, height);
+
+		const canvas = new Canvas(302, 302);
+		const ctx = canvas.getContext("2d");
+
+		circle(ctx, 0, 0, canvas.width / 2, false);
+		ctx.clip();
+
+		const width = icon.naturalWidth / shorterSide * canvas.width;
+		const height = icon.naturalHeight / shorterSide * canvas.height;
+		ctx.drawImage(icon, (canvas.width - width) / 2, (canvas.height - height) / 2, width, height);
 
 		fs.writeFileSync(`out/${twoDigits(index)}.png`, canvas.toBuffer());
 	}
